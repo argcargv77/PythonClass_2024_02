c4360c226c7d9302a5b22c47c973ecfb35d66e4a
argcargv77 <144977369+argcargv77@users.noreply.github.com>
2024-05-30 13:12:59 +0900

    example106.py 업데이트

--- example106.py
+++ example106.py
@@ -1,9 +1,15 @@
 # 파이썬에서는 여러 개의 반환값이 가능
 def add(a, b):
-	return a+b
+			return a+b
 
 def sub(a, b):
-	return a-b
+			return a-b
+
+def calculator(a, b):
+			add = a + b
+			sub = a - b
+			return add, sub			# 여러 개의 값을 반환할 경우 튜플로 생성하여 반환함.
 
 print(“합 : “, add(10, 20))
 print(“차 : ”, sub(10, 20))
+print("결과 : ", calculator(10, 20))
