4cda3dc24b7041193ec0d91d5832771ba1d5a865
argcargv77 <144977369+argcargv77@users.noreply.github.com>
2024-06-04 17:31:37 +0900

    example108.py 업데이트

--- example108.py
+++ example108.py
@@ -1,1 +1,13 @@
-...
+'''
+모듈(module): 함수의 집합
+내장(함수)모듈 : 파이썬이 기본적으로 가지고 있는 함수들
+특별한 모듈은 가져와서 사용해야 한다(import).
+'''
+import math
+import calendar
+
+number = 3.64
+print(math.ceil(number))	# 소수올림수(소수자리가 존재하면 무조건 올림)
+print(math.floor(number))	# 소수내림수(소수자리가 존재하면 무조건 내림)
+
+calendar.prmonth(2024, 6)
