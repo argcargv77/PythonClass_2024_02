b62fffc6dd83f024ccddc2c7f0e937297999836e
argcargv77 <144977369+argcargv77@users.noreply.github.com>
2024-06-04 17:37:04 +0900

    example109.py 업데이트

--- example109.py
+++ example109.py
@@ -1,1 +1,7 @@
-...
+import example107
+
+vote = [1, 2, 2, 2, 1, 2, 3, 2, 1, 2, 3, 3, 3, 3, 3, 3, 3]
+name = ["홍길동", "박지성", "김연아"]
+counter = example107.getCounter(vote, len(name))
+example107.getChart(counter, name)
+print("당선자 : %s" %example107.getName(counter, name))
