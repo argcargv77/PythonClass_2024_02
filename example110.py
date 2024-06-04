e162ff6110b233b965c6b7b4c154fffa00aaf1dd
argcargv77 <144977369+argcargv77@users.noreply.github.com>
2024-06-04 17:46:58 +0900

    example110.py 업데이트

--- example110.py
+++ example110.py
@@ -1,1 +1,10 @@
-...
+import random
+
+num1 = random.randint(1, 100)	# 임의의 정수
+
+num2 = random.random()	# 임의의 실수
+
+family = ["아빠", "엄마", "나", "동생"]
+who = random.choice(family)
+random.shuffle(family)	# 원본이 바뀜
+print(family)
